b9f510c513d9aea6e603ae20e25e5e07e0c6f9ae
gitadmin <zhenglq@yonyou.com>
2014-03-19 19:11:26 +0800

    add some file

---
+++ springORM/src/main/java/com/sunshine/spring/orm/OrmTest.java
@@ -0,0 +1,10 @@
+package com.sunshine.spring.orm;
+
+/**
+ * Description:
+ * User: sunshine
+ * Date: 14-3-19
+ * To change this template use File | Settings | File Templates.
+ */
+public class OrmTest {
+}
